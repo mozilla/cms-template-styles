eabcfd4854884f652f9c931678d60a377b1cf7cf
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-03-08 10:37:32 -0800

    refactoring preview js to pass eslint

--- src/app.js
+++ src/app.js
@@ -1,47 +1,49 @@
-$(document).ready(function(){
-  var hash = window.location.hash;
+"use strict";
 
-  if(hash.length > 0) {
-    hash = hash.replace("#","");
-    console.log(hash);
-    loadTemplate(hash);
-  } else {
-    loadTemplate("3-boxes");
+var preview = {
+  init: function () {
+    var hash = window.location.hash;
+
+    if(hash.length > 0) {
+      hash = hash.replace(`#`,``);
+      this.loadTemplate(hash);
+    } else {
+      this.loadTemplate(`3-boxes`);
+    }
+
+    $(`#template-menu`).on(`click`, `a`, (event) => {
+      this.loadTemplate($(event.target).attr(`template`));
+    });
+  },
+  loadTemplate: function (name) {
+    // Get style overrides
+    $.ajax({
+      url: `templates/${name}/style.css`,
+      success: (resp) => {
+        this.injectCSS(resp);
+      }
+    });
+
+    // Get html
+    $.ajax({
+      url: `templates/${name}/index.html`,
+      success: (resp) => {
+        this.buildContent(resp);
+      }
+    });
+  },
+  injectCSS: function (css) {
+    var style = $(`<style class='dynamic' />`);
+
+    $(`style.dynamic`).remove();
+    $(`head`).append(style);
+
+    style.html(css);
+  },
+  buildContent: function (content) {
+    $(`body > *:not(#template-menu)`).remove();
+    $(`body`).append(content);
   }
+};
 
-  $("#template-menu").on("click","a",function(){
-    loadTemplate($(this).attr("template"));
-  });
-});
-
-var less;
-function loadTemplate(name){
-
-  // Get style overrides
-  $.ajax({
-    url: "templates/"+ name+"/style.css",
-    success: function (resp) {
-      injectCSS(resp);
-    }
-  });
-
-  // Get html
-  $.ajax({
-    url: "templates/"+ name+"/index.html",
-    success: function (resp) {
-      buildContent(resp);
-    }
-  });
-}
-
-function injectCSS(css) {
-  $("style.dynamic").remove();
-  var style = $("<style class='dynamic' />");
-  $("head").append(style);
-  style.html(css);
-}
-
-function buildContent(content){
-  $("body > *:not(#template-menu)").remove();
-  $("body").append(content)
-}
+preview.init();
