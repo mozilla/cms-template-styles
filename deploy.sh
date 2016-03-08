bdcd02d270bc7421e816f71955309f0c91df07d8
Gavin Lazar Suntop <gavin@gsuntop.com>
2016-03-07 16:14:26 -0800

    adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 'Deploying Mozmaker Templates...'
+git checkout master
+git pull mozilla master
+rm -rf node_modules
+npm i
+npm run build
+git branch -D gh-pages
+git checkout --orphan gh-pages
+rm .gitignore
+echo 'node_modules/' >> .gitignore
+echo '.DS_Store' >> .gitignore
+date > last-built.txt
+git add .
+git commit -m 'AUTOMATED BUILD AND DEPLOY.'
+git push mozilla gh-pages -f
+echo 'Finished deploying Mozmaker Templates to gh-pages'
+git checkout -
